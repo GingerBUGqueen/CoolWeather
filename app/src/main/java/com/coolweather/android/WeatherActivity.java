857b3f830677312cc99d339312ff7b9e92ec1592
GingerBUGqueen <GingerBUGqueen@github.com>
2020-04-20 01:21:24 +0000

    Add Bing background

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -3,6 +3,8 @@ package com.coolweather.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -45,6 +47,16 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >=21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
         //初始化各组件
         weatherLayout = findViewById(R.id.weather_layout);
@@ -77,6 +89,7 @@ public class WeatherActivity extends AppCompatActivity {
         }else{
             loadBingPic();  // 没有读取到则加载
         }
+
     }
     /**
      * 根据天气Id请求城市天气信息
